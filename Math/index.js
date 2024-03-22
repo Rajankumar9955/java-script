77f449e67cb3b4b167d7693fc0712659b2c33e6b
Rajan <krajan92946@gmail.com>
2024-03-22 18:30:43 +0530

    Rajan

--- Math/index.js
+++ Math/index.js
@@ -1,30 +1,40 @@
-// let c=Math.pow(3,2)
-// console.log(c)
-
-// let d=Math.sqrt(13)
-// console.log(d)
-
-// let e=Math.trunc(12.565)
-// console.log(e)
-
-// let f =Math.random()*8+1 //0-1
-// console.log(f)
-
-// let r=Math.random()*8+1
-// console.log(Math.trunc(r))
-
-// let s=Math.random()*1500+10
-// console.log(Math.trunc(s))
-
 let c=Math.pow(3,2)
-console.log(c);
-let d=Math.sqrt('64')
+console.log(c)
+
+let d=Math.sqrt(13)
 console.log(d)
-let e=Math.trunc(12.2545)
+
+let e=Math.trunc(12.565)
 console.log(e)
-let f=Math.random()*8+1
+
+let f =Math.random()*8+1 //0-1
 console.log(f)
-let g=Math.random()*8+1
-console.log(Math.trunc(g))
-let h=Math.random()*1500+10
-console.log(Math.trunc(h))
+
+let r=Math.random()*8+1
+console.log(Math.trunc(r))
+
+let s=Math.random()*1500+10
+console.log(Math.trunc(s))
+
+// difference b/w floor and ceil
+
+
+
+let =tMath.floor(3.5) 
+console.log(t)
+
+let w=Math.ceil(15.1)
+console.log(w)
+
+// let c=Math.pow(3,2)
+// console.log(c);
+// let d=Math.sqrt('64')
+// console.log(d)
+// let e=Math.trunc(12.2545)
+// console.log(e)
+// let f=Math.random()*8+1
+// console.log(f)
+// let g=Math.random()*8+1
+// console.log(Math.trunc(g))
+// let h=Math.random()*1500+10
+// console.log(Math.trunc(h))
---
+++ function/interval/StartStopbuton/practice/time.js
@@ -0,0 +1,20 @@
+let time = document.getElementById('time');
+let date = document.getElementById('date');
+
+
+function start(){
+    x=setInterval(() => {
+    let a=new Date();
+    let b=a.toLocaleTimeString();
+    let c =a.toDateString();
+    time.innerHTML=b;
+    date.innerHTML=c;
+    console.log(b);
+    }, 1000);
+}
+ function stop()
+ {
+    setTimeout(()=>{
+        clearInterval(x);
+    });
+ }
