b06a160f61e6fd0ab5f0d516d7357110f3a9b124
Rajan <krajan92946@gmail.com>
2024-04-22 18:29:06 +0530

    data-base-fetching

--- API_Fetching/JSON/JSON/fetch.js
+++ API_Fetching/JSON/JSON/fetch.js
@@ -1,7 +1,25 @@
+// ``=template litral
+// async function fun()
+// {
+//   let n= await fetch("http://localhost:3000/Student")
+//   let result= await n.json() 
+//   let s=result.map((e)=>e.id)
+//   let a=result.map((e)=>e.name)
+//   document.getElementById('show').innerHTML=s;
+//   document.getElementById('show').innerHTML=a;
+
+// }
+// fun()
+// =======================================================
 async function fun()
 {
   let n= await fetch("http://localhost:3000/Student")
   let result= await n.json() 
-  console.log(result)
+  document.write(result.map((e)=>`
+      <h1> ${e.id} </h1>
+      <div> ${e.name} </div>
+      <div> ${e.city} </div>
+  `).join(" "))
+
 }
 fun()
