2483260136c8dd7628c7b20b98a63b0e97ef458a
Rajan <krajan92946@gmail.com>
2024-04-24 00:56:35 +0530

    Rajan

--- API_Fetching/Image fetching/image_fetching/image.js
+++ API_Fetching/Image fetching/image_fetching/image.js
@@ -1,18 +1,37 @@
-async function demo()
+// async function demo()
+// {
+//     let a = await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+//     let n=await a.json()
+//     let t=""
+//     let r = document.getElementById("display_data")
+//     let b= n.map((e)=>t+=
+//  `
+//    <tr>
+//    <td> ${e.id}</td>
+//    <td> ${e.albumId}</td>
+//    <td> ${e.title}</td>
+//    <td> <img src="${e.url}"</td>
+//  `
+// ).join(" ")
+// r.innerHTML=b;
+// }
+// demo()
+
+async function fun()
 {
-    let a = await fetch("https://jsonplaceholder.typicode.com/photos")
-    let n=await a.json()
-    let t=""
-    let r = document.getElementById("display_data")
-    let b= n.map((e)=>t+=
-`
-   <tr>
-   <td> ${e.id}</td>
-   <td> ${e.albumId}</td>
-   <td> ${e.title}</td>
-   <td> <img src="${e.Url}"</td>
-`
-).join(" ")
-r.innerHTML=b;
+    let n= await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+    let p=await n.json()
+    let s=""
+    let q=document.getElementById("data")
+    let v=p.map((e)=>s+=`
+    
+    <tr>
+    <td>${e.id}</td>
+    <td>${e.albumId}</td>
+    <td>${e.title}</td>
+    <td><img src="${e.url}"</td>
+
+    `).join()
+    q.innerHTML=v;
 }
-demo()
+fun()
